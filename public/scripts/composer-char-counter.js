873d04549ce69fc54035d9becbc737449ad63383
Cvanimschoot <clayton.vanimschoot@gmail.com>
2022-11-14 17:37:18 -0700

    Add tweet letter count. Add style to tweet text.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  console.log("I'm ready");
+  $("#tweet-text").on('keyup', function() {
+    const counter = $('.counter');
+    let characterCount = $(this).val().length;
+    let currentCount = 140 - characterCount;
+
+    counter.text(currentCount);
+
+    if(currentCount < 0) {
+      counter.css('color', 'red');
+    } else {
+      counter.css('color', '#545149');
+    }
+  })
+}); 
